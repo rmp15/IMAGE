f1898666c3fe73f8db9e027050d4471746b7b98d
rmp15 <r.parks15@imperial.ac.uk>
2018-05-25 10:51:40 +0100

    added stuff

---
+++ prog/00_bash/run_euro.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this script
+# run Euro-Cordex IMAGE
+
+clear
+
+#################################################
+# 1. WEIGHTED MEAN CALCULATION
+#################################################
+
+#echo "indentifying overlap between rasters and counties for pollution processing";
+#
+#for year in "${years[@]}"; do
+#for dname in "${dnames[@]}"; do
+
+# identifies the overlap between grids and counties and creates weighted means
+# MATLAB SCRIPT HERE
+
+#done; done;
