0014364a7496da42f230bc677eff0b25b6a2a0a3
rmp15 <r.parks15@imperial.ac.uk>
2018-06-22 09:31:27 +0100

    extracting values from clagarmi script

--- prog/02_processing_CLaGARMi/figure_2_processing.py
+++ prog/02_processing_CLaGARMi/figure_2_processing.py
@@ -4,14 +4,13 @@
 # processes monthly means for each of the 30-year ensembles of IMAGE data
 
 from prog.functions.data.process_clag_stats_functions import *
-import matplotlib.pyplot as plt
 
 # loading data for both observations and simulations
 obs_data, sim_data = load_clag_output('01', 300, 'euro', 'hist', 1971, 2000, 'tasmax')
 
 # processing monthly means for the CORDEX observation data
-obs_data_processed = monthly_summary(obs_data)
-sim_data_processed = monthly_summary(sim_data)
+obs_data_processed = monthly_summary(obs_data, 30, 0)
+sim_data_processed = monthly_summary(sim_data, 30, 0)
 obs_data_processed.columns = ['month', 'site', 'value_obs']
 sim_data_processed.columns = ['month', 'site', 'value_sim']
 
--- prog/functions/data/process_clag_stats_functions.py
+++ prog/functions/data/process_clag_stats_functions.py
@@ -48,7 +48,7 @@ def monthly_data(var):
 
 
 # this function will cycle through each site per month and find the mean value of a defined ensemble
-def monthly_summary(var,ens_length):
+def monthly_summary(var, ens_length, ob_sim):
 
     # load data and number of years
     data, no_years, no_sites = monthly_data(var)
@@ -57,16 +57,57 @@ def monthly_summary(var,ens_length):
     no_ens = int(no_years / ens_length)
 
     # number of sites
-    # no_sites = len(data[1][1, :])
-    no_sites = no_sites
+    no_sites = no_sites  # no_sites = len(data[1][1, :])
+
+    # 1. calculate average for entire period
+
+    print('Processing all values together ')
 
     # create empty frame to populate with average values per month at each site
-    data_avg = pd.DataFrame(columns=['month', 'site', 'value'])
+    data_avg = pd.DataFrame(columns=['month', 'site', 'mean_value', 'sd_value'])
+    # cycle through months
     for month in range(0, 12):
+        # cycle through sites
         for site in range(0, no_sites):
             mean_value = np.mean(data[month][:, site])
-            data_append = pd.DataFrame({'month': month, 'site': site, 'value': mean_value}, index=[0])
+            sd_value = np.std(data[month][:, site])
+            data_append = pd.DataFrame({'month': int(month+1), 'site': int(site+1), 'mean_value': mean_value,
+                                        'sd_value':sd_value}, index=[0])
             data_avg = pd.concat([data_avg, data_append])
 
-    return data_avg
+    if ob_sim == 1:
+
+        # 2. calculate average for ens_length-year ensembles
+
+        # create empty frame to populate with average values per month at each site
+        data_avg_ens = pd.DataFrame(columns=['month', 'site', 'ens', 'mean_value', 'sd_value'])
+        for k in range(0,no_ens):
+
+            print('Processing ensemble ' + str(k + 1) + ' of ' + str(no_ens))
+
+            # create empty frame to populate with average values per month at each site in particular ensemble
+            data_avg_ens_working = pd.DataFrame(columns=['month', 'site', 'ens', 'mean_value', 'sd_value'])
+            # cycle through months
+            for month in range(0, 12):
+
+                # number of days in each month
+                month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+                # starting position of ensemble slice in particular month
+                slice_size = month_days[month] * ens_length
+
+                # cycle through sites
+                for site in range(0, no_sites):
+                    mean_value = np.mean(data[month][k*slice_size:(k+1)*slice_size, site])
+                    sd_value = np.std(data[month][k*slice_size:(k+1)*slice_size, site])
+                    data_append = pd.DataFrame({'month': int(month+1), 'site': int(site+1), 'ens': int(k+1),
+                                                'mean_value': mean_value, 'sd_value': sd_value}, index=[0])
+                    data_avg_ens_working = pd.concat([data_avg_ens_working, data_append])
+
+            # append to master ensemble file
+            data_avg_ens = pd.concat([data_avg_ens,data_avg_ens_working])
+
+        return data_avg, data_avg_ens
+
+    if ob_sim == 0:
+        return data_avg
 
