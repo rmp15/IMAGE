8d5eacb2b758f1bbd6a65533eda7e8031ae7ab3b
rmp15 <r.parks15@imperial.ac.uk>
2018-07-05 17:37:11 +0100

    extracting values from clagarmi script

--- prog/00_bash/euro/04_plot_euro.sh
+++ prog/00_bash/euro/04_plot_euro.sh
@@ -7,6 +7,15 @@ clear
 
 #cd ~/git/IMAGE/prog/CLaGARMi/
 
+# arguments for processing of files
+declare slice='01'
+declare -a years_sims=(4000)
+declare -a metrics=('tasmax') # 'huss' 'sfcWindmax')
+declare -a continents=('euro')
+declare -a scens=('hist')
+declare -i start=1971
+declare -i end=2000
+
 #################################################
 # 1. EURO-CORDEX RUNS
 #################################################
