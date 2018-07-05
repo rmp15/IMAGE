c0a47506ffbf168d2b6628145c8e83c5b975e9bc
rmp15 <r.parks15@imperial.ac.uk>
2018-07-05 18:35:03 +0100

    added plots edits

--- prog/00_bash/euro/04_plot_euro.sh
+++ prog/00_bash/euro/04_plot_euro.sh
@@ -9,7 +9,8 @@ clear
 
 # arguments for processing of files
 declare slice='01'
-declare -a years_sims=(4000)
+declare -y years_sim1=6000
+declare -y years_sim2=4000
 declare -a metrics=('tasmax') # 'huss' 'sfcWindmax')
 declare -a continents=('euro')
 declare -a scens=('hist')
@@ -20,4 +21,10 @@ declare -i end=2000
 # 1. EURO-CORDEX RUNS
 #################################################
 
-python ~/git/IMAGE/prog/02_processing_CLaGARMi/figures.py
+for metric in "${metrics[@]}"; do
+for continent in "${continents[@]}"; do
+for scen in "${scens[@]}"; do
+
+Rscript ~/git/IMAGE/prog/02_processing_CLaGARMi/figures.R $slice $years_sim1 $years_sim2 $metric $continent $scen $start $end
+
+done; done; done;
--- prog/03_plotting_CLaGARMi/figures.R
+++ prog/03_plotting_CLaGARMi/figures.R
@@ -1,11 +1,23 @@
+rm(list=ls())
+
 library(ggplot2)
 library(plyr)
 
-# TEMPORARY
-month.short <- c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
+# break down the arguments from Rscript
+args <- commandArgs(trailingOnly=TRUE)
+slice <- as.character(args[1])
+years_sim1 <- as.numeric(args[2])
+years_sim2 <- as.numeric(args[3])
+metric <- as.character(args[4])
+continent <- as.character(args[5])
+scen <- as.character(args[6])
+start <- as.numeric(args[7])
+end <- as.numeric(args[8])
 
-# need to generalise to any location any ensemble etc.
-var = 'huss'
+years_sim_tot = years_sim1 + years_sim2
+
+# short names for months
+month.short <- c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
 
 input.dir = '~/git/IMAGE/output/CLaGARMi/euro_cordex/figures_processing/'
 output.dir = '~/git/IMAGE/output/CLaGARMi/euro_cordex/figures/'
@@ -14,11 +26,40 @@ output.dir = '~/git/IMAGE/output/CLaGARMi/euro_cordex/figures/'
 # Figure 2
 #################################
 
-# load data for both comparing entire sim and obs, as well as sim ensemble data GENERALISE
-dat.obs.sim = read.csv(paste0(input.dir,'/',var,'_euro_hist_1971_2000_obs_sim_merged.csv'))
-dat.sim.ens = read.csv(paste0(input.dir,'/',var,'_euro_hist_1971_2000_sim_ens.csv'))
-dat.sim.ens = ddply(dat.sim.ens,.(month,site),summarise,mean_max=max(mean_value), mean_min=min(mean_value),
+# load data for both comparing entire sim and obs
+dat.obs.sim.1 = read.csv(paste0(input.dir,metric,'_',continent,'_',scen,'_',start,'_',end,'_',years_sim1,'yrs__obs_sim_merged.csv'))
+dat.obs.sim.2 = read.csv(paste0(input.dir,metric,'_',continent,'_',scen,'_',start,'_',end,'_',years_sim2,'yrs__obs_sim_merged.csv'))
+dat.obs.sim.1$mean_value_sim1 = dat.obs.sim.1$mean_value_sim ; dat.obs.sim.1$mean_value_sim = NULL
+dat.obs.sim.2$mean_value_sim2 = dat.obs.sim.2$mean_value_sim ; dat.obs.sim.2$mean_value_sim = NULL
+dat.obs.sim.1$sd_value_sim1 = dat.obs.sim.1$sd_value_sim ; dat.obs.sim.1$sd_value_sim = NULL
+dat.obs.sim.2$sd_value_sim2 = dat.obs.sim.2$sd_value_sim ; dat.obs.sim.2$sd_value_sim = NULL
+
+dat.obs.sim = merge(dat.obs.sim.1,dat.obs.sim.2)
+
+dat.obs.sim$mean_value_sim = (dat.obs.sim$mean_value_sim1*years_sim1 + dat.obs.sim$mean_value_sim2*years_sim2)/(years_sim1+years_sim2)
+dat.obs.sim$sd_value_sim = (dat.obs.sim$sd_value_sim1*years_sim1 + dat.obs.sim$sd_value_sim2*years_sim2)/(years_sim1+years_sim2)
+
+# remove unnecessary columns
+dat.obs.sim$mean_value_sim1 = dat.obs.sim$mean_value_sim2 = dat.obs.sim$sd_value_sim1 = dat.obs.sim$sd_value_sim2= NULL
+dat.obs.sim$X = NULL
+
+# order by month and by site and fix row names
+dat.obs.sim = dat.obs.sim[order(dat.obs.sim$month,dat.obs.sim$site),]
+rownames(dat.obs.sim) = seq(1:nrow(dat.obs.sim))
+
+# also load sim ensemble data and find mins and max from total combined years
+dat.sim.ens.1 = read.csv(paste0(input.dir,metric,'_',continent,'_',scen,'_',start,'_',end,'_',years_sim1,'yrs__sim_ens.csv'))
+dat.sim.ens.2 = read.csv(paste0(input.dir,metric,'_',continent,'_',scen,'_',start,'_',end,'_',years_sim2,'yrs__sim_ens.csv'))
+
+dat.sim.ens.1 = ddply(dat.sim.ens.1,.(month,site),summarise,mean_max=max(mean_value), mean_min=min(mean_value),
                     sd_max=max(sd_value),sd_min=min(sd_value))
+dat.sim.ens.2 = ddply(dat.sim.ens.2,.(month,site),summarise,mean_max=max(mean_value), mean_min=min(mean_value),
+                    sd_max=max(sd_value),sd_min=min(sd_value))
+
+dat.sim.ens = rbind(dat.sim.ens.1,dat.sim.ens.2)
+dat.sim.ens = ddply(dat.sim.ens,.(month,site),summarise,mean_max=max(mean_max),mean_min=min(mean_min),
+                    sd_max=max(sd_max),sd_min=min(sd_min))
+
 dat.sim.ens = merge(dat.obs.sim,dat.sim.ens,by=c('month','site'),all.x=0)
 
 # add short names of months
@@ -27,12 +68,12 @@ dat.obs.sim$month.short <- reorder(dat.obs.sim$month.short,dat.obs.sim$month)
 dat.sim.ens$month.short <- mapvalues(dat.sim.ens$month,from=sort(unique(dat.sim.ens$month)),to=month.short)
 dat.sim.ens$month.short <- reorder(dat.sim.ens$month.short,dat.sim.ens$month)
 
-pdf(paste0(output.dir,var,'_mean.pdf'),paper='a4r',height=0,width=0)
+pdf(paste0(output.dir,metric,'_',continent,'_',scen,'_',start,'_',end,'_',years_sim_tot,'yrs_mean.pdf'),paper='a4r',height=0,width=0)
 print(ggplot() +
     geom_point(data=dat.obs.sim,aes(x=mean_value_obs,y=mean_value_sim),size=0.5) +
     geom_errorbar(data=dat.sim.ens,aes(x=mean_value_obs,ymax=mean_max,ymin=mean_min),alpha=0.2,col='dark blue') +
     geom_abline(col='red',alpha='0.5') +
-    ggtitle(paste0(var,' mean')) +
+    ggtitle(paste0(metric,' mean')) +
     xlab('CORDEX') + ylab('IMAGE simulated') +
     facet_wrap(~month.short,scale='free') +
     theme_bw() + theme(panel.grid.major = element_blank(),axis.text.x = element_text(angle=0),
@@ -47,12 +88,12 @@ dev.off()
 # Figure 3
 #################################
 
-pdf(paste0(output.dir,var,'_sd.pdf'),paper='a4r',height=0,width=0)
+pdf(paste0(output.dir,metric,'_',continent,'_',scen,'_',start,'_',end,'_',years_sim_tot,'yrs_sd.pdf'),paper='a4r',height=0,width=0)
 print(ggplot() +
     geom_point(data=dat.obs.sim,aes(x=sd_value_obs,y=sd_value_sim),size=0.5) +
     geom_errorbar(data=dat.sim.ens,aes(x=sd_value_obs,ymax=sd_max,ymin=sd_min),alpha=0.2,col='dark blue') +
     geom_abline(col='red',alpha='0.5') +
-    ggtitle(paste0(var,' sd')) +
+    ggtitle(paste0(metric,' sd')) +
     xlab('CORDEX') + ylab('IMAGE simulated') +
     facet_wrap(~month.short,scale='free') +
     theme_bw() + theme(panel.grid.major = element_blank(),axis.text.x = element_text(angle=0),
