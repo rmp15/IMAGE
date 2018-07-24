aac655c8aa1cd64d97a4e46bae1400513c216d86
rmp15 <r.parks15@imperial.ac.uk>
2018-07-24 11:44:13 +0100

    extracting values from clagarmi script

--- prog/CLaGARMi/run_CLaGARMi_euro_hist.m
+++ prog/CLaGARMi/run_CLaGARMi_euro_hist.m
@@ -6,7 +6,7 @@ addpath('~/git/IMAGE/prog/01_extract_files/');
 addpath('../../data/CORDEX_nobc_clagarm_input/')
 addpath('../../prog/CLaGARMi/v1')
 
-var_names = {'tasmax','huss','sfcWindmax'};
+var_names = {'tasmax','huss','sfcWindmax', 'ps'};
 
 % generalise loading process
 load /home/rmp15/data/IMAGE/CORDEX/euro_cordex/tasmax/processed/tasmax_MPI-M-MPI-ESM-LR_historical_r2i1p1_MPI-CSC-REMO2009_v119712000.mat
@@ -15,13 +15,15 @@ load /home/rmp15/data/IMAGE/CORDEX/euro_cordex/huss/processed/huss_MPI-M-MPI-ESM
 mv(2)=v;
 load /home/rmp15/data/IMAGE/CORDEX/euro_cordex/sfcWindmax/processed/sfcWindmax_MPI-M-MPI-ESM-LR_historical_r2i1p1_MPI-CSC-REMO2009_v119712000.mat
 mv(3)=v;
+load /home/rmp15/data/IMAGE/CORDEX/euro_cordex/sfcWindmax/processed/ps_MPI-M-MPI-ESM-LR_historical_r2i1p1_MPI-CSC-REMO2009_v119712000.mat
+mv(4)=v;
 clear v
 
 % ClaGARMi will save its output in 'sroot' folder, also AR fits and Cholesky
 % decomp of AR fits get saved her for improved re-run time
 sroot='~/data/IMAGE/CLaGARMi/euro_cordex_output/';
 
-nyrs=4000; % simulation length
+nyrs=6000; % simulation length
 nmnths=12; % no 'months' per year
 niters=10; % no. iterations on residual convariance matrix
 split=1; %used to split very long runs into smaller chunks
@@ -55,3 +57,10 @@ sfcWindmax_s = mv(3).s;
 sfcWindmax_o = mv(3).o;
 save(sfcWindmax_s_fn,'sfcWindmax_s','-v7.3');
 save(sfcWindmax_o_fn,'sfcWindmax_o','-v7.3');
+
+ps_s_fn = strcat(sroot,'ps/',savefilename,'_ps_s');
+ps_o_fn = strcat(sroot,'ps/',savefilename,'_ps_o');
+ps_s = mv(4).s;
+ps_o = mv(4).o;
+save(ps_s_fn,'ps_s','-v7.3');
+save(ps_o_fn,'ps_o','-v7.3');
