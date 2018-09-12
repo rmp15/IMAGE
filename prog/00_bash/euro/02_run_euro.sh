3766272ca5a7a94e8ff4362302745d7be77ad64b
rmp15 <r.parks15@imperial.ac.uk>
2018-09-12 19:55:17 -0400

    extracting values from clagarmi script

--- prog/00_bash/euro/02_run_euro.sh
+++ prog/00_bash/euro/02_run_euro.sh
@@ -14,19 +14,20 @@ cd ~/git/IMAGE/prog/CLaGARMi/
 (
 
 nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_6000yr ;
-#nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_4000yr ;
+#nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_4000yr;
+
+#nohup matlab -nosplash -nojvm -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_6000yr ;
+#nohup matlab -nosplash -nojvm -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_4000yr ;
+
+#nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_6000yr ;
+#nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_4000yr ;
 
 #nohup matlab -nosplash -nojvm -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_6000yr ;
 #nohup matlab -nosplash -nojvm -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_4000yr ;
 
-#nohup matlab -nosplash -nojvm -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_6000yr ;
-#nohup matlab -nosplash -nojvm -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_4000yr ; running also but need to check
-
 #nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_6000yr ;
 #nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_4000yr ;
 
-#nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_6000yr ;
-#nohup matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_4000yr ;
 :
 ) &
 
