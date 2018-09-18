12e9a20001e111d481845c474bf4d10d44c69f21
rmp15 <rparks15@imperial.ac.uk>
2018-09-18 15:46:26 +0100

    extracting values from clagarmi script

--- prog/CLaGARMi/test.m
+++ prog/CLaGARMi/test.m
@@ -1,10 +1,14 @@
 % Loads Euro CORDEX historical data and then runs CLaGARMi
 
+function []=test(arg1,arg2)
+
 clear
 
 addpath('~/git/IMAGE/prog/01_extract_files/');
 addpath('../../data/CORDEX_nobc_clagarm_input/')
 addpath('../../prog/CLaGARMi/v1')
 
-s=2;
-disp(s);
+%s=2;
+disp(arg1);
+
+end
