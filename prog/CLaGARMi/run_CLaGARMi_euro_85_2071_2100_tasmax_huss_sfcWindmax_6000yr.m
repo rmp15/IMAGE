9f3999d79dd59d669bf2f53ead14f33fc78e608c
rmp15 <r.parks15@imperial.ac.uk>
2018-09-23 11:39:06 +0100

    extracting values from clagarmi script

--- prog/CLaGARMi/run_CLaGARMi_euro_85_2071_2100_tasmax_huss_sfcWindmax_6000yr.m
+++ prog/CLaGARMi/run_CLaGARMi_euro_85_2071_2100_tasmax_huss_sfcWindmax_6000yr.m
@@ -25,11 +25,11 @@ nyrs=6000; % simulation length
 nmnths=12; % no 'months' per year
 niters=10; % no. iterations on residual convariance matrix
 split=1; %used to split very long runs into smaller chunks
-savefilename = strcat('out_',num2str(split,'%02d'),'_y',int2str(nyrs),'_euro_rcp85_2021_2050_',output_label);
+savefilename = strcat('out_',num2str(split,'%02d'),'_y',int2str(nyrs),'_euro_rcp85_2071_2100_',output_label);
 
 scen        = 'rcp85';
-starty      = '2021';
-endy        = '2050';
+starty      = '2071';
+endy        = '2100';
 var_names   = 'tasmax_huss_sfcWindmax';
 
 tic
