df2a140e55d61337ac8e85a9384e7bf1735afb1a
rmp15 <r.parks15@imperial.ac.uk>
2018-09-27 16:51:40 +0100

    extracting values from clagarmi script

--- prog/00_bash/euro/02_run_euro.sh
+++ prog/00_bash/euro/02_run_euro.sh
@@ -13,9 +13,9 @@ cd ~/git/IMAGE/prog/CLaGARMi/
 
 (
 
-#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_tasmax_huss_sfcWindmax_6000yr;
+matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_tasmax_huss_sfcWindmax_6000yr;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_tasmax_huss_sfcWindmax_4000yr;
-#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_6000yr;
+matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_6000yr;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_4000yr;
 
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_tasmax_huss_sfcWindmax_6000yr ;
@@ -24,19 +24,19 @@ cd ~/git/IMAGE/prog/CLaGARMi/
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_pr_ps_4000yr ;
 
 matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_tasmax_huss_sfcWindmax_6000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_tasmax_huss_sfcWindmax_4000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_pr_ps_6000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_pr_ps_4000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_tasmax_huss_sfcWindmax_4000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_pr_ps_6000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_pr_ps_4000yr ;
 
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_tasmax_huss_sfcWindmax_6000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_tasmax_huss_sfcWindmax_4000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_pr_ps_6000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_pr_ps_4000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_tasmax_huss_sfcWindmax_6000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_tasmax_huss_sfcWindmax_4000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_pr_ps_6000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2021_2050_pr_ps_4000yr ;
 
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_tasmax_huss_sfcWindmax_6000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_tasmax_huss_sfcWindmax_4000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_pr_ps_6000yr ;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_pr_ps_4000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_tasmax_huss_sfcWindmax_6000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_tasmax_huss_sfcWindmax_4000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_pr_ps_6000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2021_2050_pr_ps_4000yr ;
 
 :
 ) &
