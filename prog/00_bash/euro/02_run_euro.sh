6a1cd2f9240eee0f0736094373097d7f3fbbb9eb
rmp15 <r.parks15@imperial.ac.uk>
2018-10-04 22:19:17 +0100

    extracting values from clagarmi script

--- prog/00_bash/euro/02_run_euro.sh
+++ prog/00_bash/euro/02_run_euro.sh
@@ -13,9 +13,9 @@ cd ~/git/IMAGE/prog/CLaGARMi/
 
 (
 
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_tasmax_huss_sfcWindmax_6000yr;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_tasmax_huss_sfcWindmax_6000yr;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_tasmax_huss_sfcWindmax_4000yr;
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_6000yr;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_6000yr;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_4000yr;
 
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_tasmax_huss_sfcWindmax_6000yr ;
@@ -23,7 +23,7 @@ matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_hist_pr_ps_6000
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_pr_ps_6000yr ;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_85_2071_2100_pr_ps_4000yr ;
 
-matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_tasmax_huss_sfcWindmax_6000yr ;
+#matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_tasmax_huss_sfcWindmax_6000yr ;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_tasmax_huss_sfcWindmax_4000yr ;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_pr_ps_6000yr ;
 #matlab -nosplash -nodesktop -softwareopengl -r run_CLaGARMi_euro_45_2071_2100_pr_ps_4000yr ;
