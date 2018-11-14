c5154d376f881498604191ed5584e366a1185fdb
rmp15 <r.parks15@imperial.ac.uk>
2018-11-14 12:41:11 -0500

    extracting values from clagarmi script

---
+++ prog/02_processing_CLaGARMi/CLaGARMi_spi_process.m
@@ -0,0 +1,16 @@
+% this program
+% takes output of CLaGARMi
+% calculates SPI for each site
+
+% go to correct directory
+cd '~/data/IMAGE/CLaGARMi/euro_cordex_output/pr/'
+
+% load fiveyear_filereader for processing CORDEX files
+addpath('~/git/IMAGE/prog/02_processing_CLaGARMi/');
+
+% load the desired version of
+
+
+
+
+
