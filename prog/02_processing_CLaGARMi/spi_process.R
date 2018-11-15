348aacb345ab96b2b8ac3973246b1356e7c13a84
rmp15 <r.parks15@imperial.ac.uk>
2018-11-15 18:40:25 -0500

    extracting values from clagarmi script

--- prog/02_processing_CLaGARMi/spi_process.R
+++ prog/02_processing_CLaGARMi/spi_process.R
@@ -43,12 +43,12 @@ tasmax.sim = np$load(file.loc.tasmax.sim)
 
 # test take one site and calculate SPI as test
 site = pr.hist[1,,]
-site.vector = as.vector(t(site))
+site.vector.obs = as.vector(t(site))
 
 # create dataframe with year, month, precipitation
 year.seq = rep(1:30, each=365)
 month.seq = c(rep(1,31),rep(2,28),rep(3,31),rep(4,30),rep(5,31),rep(6,30),rep(7,31),rep(8,31),rep(9,30),rep(10,31),rep(11,30),rep(12,31))
-dat.site = data.frame(year=year.seq,month=month.seq,precip=site.vector)
+dat.site = data.frame(year=year.seq,month=month.seq,precip=site.vector.obs)
 
 # summarise precipitation by month
 dat.site.summarised = ddply(dat.site,.(year,month),summarise,precip=sum(precip))
@@ -69,14 +69,14 @@ spi_12 <- spi(dat.site.summarised[,'precip'], 12)
 # NEED TO FIX A REFERENCE PERIOD using ref.start and ref.end in the arguments of
 
 site = pr.sim[1,,]
-site.vector = as.vector(t(site))
+site.vector.sim = as.vector(t(site))
 
 num.years = dim(site)[1]
 
 # create dataframe with year, month, precipitation
 year.seq = rep(1:num.years, each=365)
 month.seq = c(rep(1,31),rep(2,28),rep(3,31),rep(4,30),rep(5,31),rep(6,30),rep(7,31),rep(8,31),rep(9,30),rep(10,31),rep(11,30),rep(12,31))
-dat.site = data.frame(year=year.seq,month=month.seq,precip=site.vector)
+dat.site = data.frame(year=year.seq,month=month.seq,precip=site.vector.sim)
 
 # summarise precipitation by month
 dat.site.summarised = ddply(dat.site,.(year,month),summarise,precip=sum(precip))
@@ -91,4 +91,30 @@ library(ggplot2)
 # use SPEI program to calculate SPI
 spi_1 <- spi(dat.site.summarised[,'precip'], 1)
 spi_3 <- spi(dat.site.summarised[,'precip'], 3)
+spi_12 <- spi(dat.site.summarised[,'precip'], 12)
+
+# SIM DATA WITH FIXED REFERENCE PERIOD
+
+# create dataframe with year, month, precipitation
+year.seq.long = rep(31:(num.years+30), each=365)
+month.seq = c(rep(1,31),rep(2,28),rep(3,31),rep(4,30),rep(5,31),rep(6,30),rep(7,31),rep(8,31),rep(9,30),rep(10,31),rep(11,30),rep(12,31))
+
+year.seq.long = c(year.seq, year.seq.long)
+site.vector.long = c(site.vector.obs, site.vector.sim)
+
+dat.site = data.frame(year=year.seq.long,month=month.seq,precip=site.vector.long)
+
+# summarise precipitation by month
+dat.site.summarised = ddply(dat.site,.(year,month),summarise,precip=sum(precip))
+
+# convert to mm m-2 s-1 (currently in kg m-2 s-1) https://www.convertunits.com/from/kg/cm2/to/water+column+[millimeter]
+# scale factor is 10,000
+dat.site.summarised$precip = 10000 * dat.site.summarised$precip
+
+library(ggplot2)
+# ggplot(data=dat.site.summarised) + geom_line(aes(x=year,y=precip,group=month,color=month)) + facet_wrap(~month)
+
+# use SPEI program to calculate SPI
+spi_1 <- plot(spi(dat.site.summarised[,'precip'], 1, ref.start=c(1,1), ref.end=c(30,12)))
+spi_3 <- spi(dat.site.summarised[,'precip'], 3, ref.start=c(1,1), ref.end=c(30,12))
 spi_12 <- spi(dat.site.summarised[,'precip'], 12)
