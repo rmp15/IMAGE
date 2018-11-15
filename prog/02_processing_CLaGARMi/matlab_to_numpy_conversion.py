50f41f86d97a6a5f2a436d1fe2362dc5ead2df8c
rmp15 <r.parks15@imperial.ac.uk>
2018-11-15 11:23:23 -0500

    extracting values from clagarmi script

--- prog/02_processing_CLaGARMi/matlab_to_numpy_conversion.py
+++ prog/02_processing_CLaGARMi/matlab_to_numpy_conversion.py
@@ -25,8 +25,9 @@ season_start = int(float((sys.argv[8])))        # season_start = 5
 season_end = int(float((sys.argv[9])))          # season_end = 9
 percentile = int(float((sys.argv[10])))         # percentile = 99
 
+# only for when inputting manually
 #slice = '01'; years_sim_1 = 4000 ; years_sim_2 = 6000; metric = 'tasmax'; continent = 'euro'; scen = 'hist'
-year_start = 1971; year_end = 2000; season_start = 5; season_end = 9; percentile = 99
+# year_start = 1971; year_end = 2000; season_start = 5; season_end = 9; percentile = 99
 
 # file loc in case its being run on linux platform
 if sys.platform == 'linux' or sys.platform == 'linux2':
