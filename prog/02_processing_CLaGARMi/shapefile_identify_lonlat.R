f5709f31a3f71aa48596d457c31b43d7f2499951
rmp15 <r.parks15@imperial.ac.uk>
2018-11-22 11:44:22 +0000

    extracting values from clagarmi script

--- prog/02_processing_CLaGARMi/shapefile_identify_lonlat.R
+++ prog/02_processing_CLaGARMi/shapefile_identify_lonlat.R
@@ -1,10 +1,14 @@
 rm(list=ls())
 
+# arguments to finish
+contintent = 'euro'
+
 library(plyr)
 library(reticulate)
 library(ggplot2)
 library(maptools)
 library(mapproj)
+library(graticule)
 library(rgeos)
 library(rgdal)
 library(RColorBrewer)
@@ -38,5 +42,38 @@ theme_map <- function(base_size=9, base_family=""){
 
 # load shapefile
 map <- readOGR(dsn="../../data/shapefiles/ne_10m_admin_0_countries",layer="ne_10m_admin_0_countries")
+original.proj <- proj4string(map)
+euro <- map[map$CONTINENT %in% c("Europe"),]
+
+# function to isolate single country polygon to finish
+france = euro[euro$GEOUNIT %in% c("France"),]
+spain = euro[euro$GEOUNIT %in% c("Spain"),]
+
+# fortify to prepare for ggplot
+euro.fortify <- fortify(euro)
+spain.fortify = fortify(spain)
 
 # load lonlat file for IMAGE
+grid = read.csv('~/git/IMAGE/output/CLaGARMi/euro_cordex/lonlat/euro_lonlat.csv')
+names(grid)[1] = 'ID'
+
+# plot for refrence of grid IDs
+# ggplot() + geom_text(data=grid,aes(x=lon,y=lat,label=ID),size=3)
+
+# fix where western Mallorca should be in the map to fix the grid points
+# western Mallorca is  ID = 104, lon = -12.15, lat = -9.79
+# real value is lon = 3, lat = 39.6
+# grid$lon = grid$lon + 15.15
+# grid$lat = grid$lat + (39.6+9.79)
+
+# fix where London should be on the map
+
+
+# create a graticule from grid points for Europe
+grat <- graticule(lons=grid$lon,lats=grid$lat,xlim=c(min(grid$lon),max(grid$lon)),ylim=c(min(grid$lat),max(grid$lat)),proj=projection(original.proj))
+
+# plot fortified europe map
+ggplot() +
+    geom_text(data=grat,aes(x=lon,y=lat),size=3) +
+    geom_polygon(data=subset(spain.fortify),aes(x=long,y=lat,group=group),linetype=2,size=0)
+
