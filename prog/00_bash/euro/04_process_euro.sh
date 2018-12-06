a8b1dff51fa73c85aa87af16843e79272c5d84c3
rmp15 <r.parks15@imperial.ac.uk>
2018-12-06 14:22:11 +0000

    extracting values from clagarmi script

--- prog/00_bash/euro/04_process_euro.sh
+++ prog/00_bash/euro/04_process_euro.sh
@@ -13,9 +13,9 @@ declare slice='01'
 declare -a years_sims=(4000 6000)
 declare -a metrics=('huss') # 'huss' 'sfcWindmax')
 declare -a continents=('euro')
-declare -a scens=('hist')
-declare -i start=1971
-declare -i end=2000
+declare -a scens=('rcp45' 'rcp85')
+declare -i start=2021
+declare -i end=2050
 
 #################################################
 # 1. EURO-CORDEX RUNS
@@ -27,8 +27,17 @@ for continent in "${continents[@]}"; do
 for scen in "${scens[@]}"; do
 
 #python ~/git/IMAGE/prog/02_processing_CLaGARMi/figure_2_3_processing.py $slice $years_sim $metric $continent $scen $start $end
-python3.6 -m prog.02_processing_CLaGARMi.app_temp_process $slice $years_sim $continent $scen $start $end
 
 done; done; done; done;
+
+
+for years_sim in "${years_sims[@]}"; do
+for continent in "${continents[@]}"; do
+for scen in "${scens[@]}"; do
+
+#python ~/git/IMAGE/prog/02_processing_CLaGARMi/figure_2_3_processing.py $slice $years_sim $metric $continent $scen $start $end
+python3.6 -m prog.02_processing_CLaGARMi.app_temp_process $slice $years_sim $continent $scen $start $end
+
+done; done; done;
 
 #python ~/git/IMAGE/prog/02_processing_CLaGARMi/figure_8_processing.py $slice $years_sim $metric $continent $scen $start $end
