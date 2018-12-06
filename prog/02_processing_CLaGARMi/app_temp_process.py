8d889aea0e9fe8fa6d2393aa26f0663d7f78fec1
rmp15 <r.parks15@imperial.ac.uk>
2018-12-06 13:45:24 +0000

    extracting values from clagarmi script

--- prog/02_processing_CLaGARMi/app_temp_process.py
+++ prog/02_processing_CLaGARMi/app_temp_process.py
@@ -20,27 +20,45 @@ year_end = int(float((sys.argv[6])))            # year_end = 2000
 
 metric = 'appt'
 
-# loading data for both observations and simulations
-tas_obs_data,  tas_sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, 'tasmax')
-huss_obs_data, huss_sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, 'huss')
-ps_obs_data, ps_sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, 'ps')
+print('loading data for tasmax, huss and ps')
 
+# loading data for both observations and simulations
+
+tas_obs_data,  tas_sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, 'tasmax')
+print('tasmax loaded')
+
+huss_obs_data, huss_sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, 'huss')
+print('huss loaded')
+
+ps_obs_data, ps_sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, 'ps')
+print('ps loaded')
+
+# process apparent temperature
+print('processing apparent temperature')
 appt_obs_data = app_temp_creator(tas_obs_data, huss_obs_data, ps_obs_data)
 appt_sim_data = app_temp_creator(tas_sim_data, huss_sim_data, ps_sim_data)
 
-# processing monthly means for the CORDEX observation data
-obs_data_processed = monthly_summary(appt_obs_data, 30, 0)
 
-# processing monthly means for the CORDEX sim data
-# with summary statistics for the entire period and for ensembles chunks
-sim_data_processed_all, sim_data_processed_ens = monthly_summary(appt_sim_data, 30, 1)
-obs_data_processed.columns = ['mean_value_obs', 'month', 'sd_value_obs', 'site']
+# names for output files
+fn_o = var + '/out_' + step + '_y' + str(num_years) + '_' + continent + '_' + str(scen_name) + '_' + str(
+    start_year) + '_' + str(end_year) + '_' + var + '_o.mat'
+fn_s = var + '/out_' + step + '_y' + str(num_years) + '_' + continent + '_' + str(scen_name) + '_' + str(
+    start_year) + '_' + str(end_year) + '_' + var + '_s.mat'
 
-sim_data_processed_all.columns = ['mean_value_sim', 'month', 'sd_value_sim', 'site']
 
-# combine two tables of complete values
-obs_sim_data_processed = pd.merge(obs_data_processed, sim_data_processed_all)
-
-# output to merged obs and sim values to csv
-obs_sim_data_processed.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + '_obs_sim_merged.csv')
-sim_data_processed_ens.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + '_sim_ens.csv')
+# # processing monthly means for the CORDEX observation data
+# obs_data_processed = monthly_summary(appt_obs_data, 30, 0)
+#
+# # processing monthly means for the CORDEX sim data
+# # with summary statistics for the entire period and for ensembles chunks
+# sim_data_processed_all, sim_data_processed_ens = monthly_summary(appt_sim_data, 30, 1)
+# obs_data_processed.columns = ['mean_value_obs', 'month', 'sd_value_obs', 'site']
+#
+# sim_data_processed_all.columns = ['mean_value_sim', 'month', 'sd_value_sim', 'site']
+#
+# # combine two tables of complete values
+# obs_sim_data_processed = pd.merge(obs_data_processed, sim_data_processed_all)
+#
+# # output to merged obs and sim values to csv
+# obs_sim_data_processed.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + '_obs_sim_merged.csv')
+# sim_data_processed_ens.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + '_sim_ens.csv')
