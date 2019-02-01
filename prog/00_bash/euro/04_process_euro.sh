17917482fd3168ec5fcd0b38d0abf3d54c8a63be
rmp15 <r.parks15@imperial.ac.uk>
2019-02-01 00:46:24 +0000

    extracting values from clagarmi script

--- prog/00_bash/euro/04_process_euro.sh
+++ prog/00_bash/euro/04_process_euro.sh
@@ -26,7 +26,6 @@ for years_sim in "${years_sims[@]}"; do
 for continent in "${continents[@]}"; do
 for scen in "${scens[@]}"; do
 
-:
 python3.6 -m prog.02_processing_CLaGARMi.app_temp_process $slice $years_sim $continent $scen $start $end
 
 done; done; done;
