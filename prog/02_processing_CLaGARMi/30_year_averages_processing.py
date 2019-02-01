22bfffac790806aa2c35ed5531b061cd54bc98c3
rmp15 <r.parks15@imperial.ac.uk>
2019-02-01 01:23:55 +0000

    extracting values from clagarmi script

--- prog/02_processing_CLaGARMi/30_year_averages_processing.py
+++ prog/02_processing_CLaGARMi/30_year_averages_processing.py
@@ -21,11 +21,17 @@ scen = sys.argv[5]                              # scen = 'hist'
 year_start = int(float((sys.argv[6])))          # year_start = 1971
 year_end = int(float((sys.argv[7])))            # year_end = 2000
 
+print('loading data for ' + metric)
+
 # loading data for both observations and simulations
 obs_data, sim_data = load_clag_output(slice, years_sim, continent, scen, year_start, year_end, metric)
 
+print('processing monthly means')
+
 # processing monthly means for the CORDEX observation data
 obs_data_processed = monthly_mean_summary(obs_data, 30, 0)
+
+print('processing monthly means')
 
 # processing monthly means for the CORDEX sim data
 # with summary statistics for the entire period and for ensembles chunks
@@ -37,5 +43,5 @@ sim_data_processed_all.columns = ['mean_value_sim', 'month', 'sd_value_sim', 'si
 obs_sim_data_processed = pd.merge(obs_data_processed, sim_data_processed_all)
 
 # output to merged obs and sim values to csv
-obs_sim_data_processed.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + '_obs_sim_merged.csv')
-sim_data_processed_ens.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + '_sim_ens.csv')
+obs_sim_data_processed.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + 'obs_sim_merged.csv')
+sim_data_processed_ens.to_csv('~/git/IMAGE/output/CLaGARMi/' + continent + '_cordex/figures_processing/' + metric + '_' + continent + '_' + scen + '_' + str(year_start) + '_' + str(year_end) + '_' + str(years_sim) + 'yrs_' + 'sim_ens.csv')
