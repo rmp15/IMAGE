9d682ab4acec5aa59cd5f114acb55a47d8978239
rmp15 <r.parks15@imperial.ac.uk>
2019-02-01 01:13:18 +0000

    extracting values from clagarmi script

--- prog/00_bash/euro/04_process_euro.sh
+++ prog/00_bash/euro/04_process_euro.sh
@@ -21,7 +21,7 @@ declare -i end=2000
 # 1. EURO-CORDEX RUNS
 #################################################
 
-# process apparent temperature
+# PROCESS APPARENT TEMPERATURE
 for years_sim in "${years_sims[@]}"; do
 for continent in "${continents[@]}"; do
 for scen in "${scens[@]}"; do
@@ -30,29 +30,33 @@ python -m prog.02_processing_CLaGARMi.app_temp_process $slice $years_sim $contin
 
 done; done; done;
 
-declare -i year_sim_1=4000
-declare -i year_sim_2=6000
-declare -i season_start=5
-declare -i season_end=9
-declare -i percentile=99
-
-# process return periods
-for metric in "${metrics[@]}"; do
-for continent in "${continents[@]}"; do
-for scen in "${scens[@]}"; do
-
-#python3.6 -m prog.02_processing_CLaGARMi.return_period_plots_processing $slice $year_sim_1 $year_sim_2 $metric $continent $scen $start $end $season_start $season_end $percentile
-
-done; done; done;
-
+# PROCESS SETS OF 30-YEAR AVERAGES FOR COMPARISON IN A FIGURE
 for years_sim in "${years_sims[@]}"; do
 for metric in "${metrics[@]}"; do
 for continent in "${continents[@]}"; do
 for scen in "${scens[@]}"; do
 
 :
-#python ~/git/IMAGE/prog/02_processing_CLaGARMi/figure_2_3_processing.py $slice $years_sim $metric $continent $scen $start $end
+python ~/git/IMAGE/prog/02_processing_CLaGARMi/figure_2_3_processing.py $slice $years_sim $metric $continent $scen $start $end
 
 done; done; done; done;
 
 #python ~/git/IMAGE/prog/02_processing_CLaGARMi/figure_8_processing.py $slice $years_sim $metric $continent $scen $start $end
+
+
+# PROCESS RETURN PERIODS
+declare -i year_sim_1=4000
+declare -i year_sim_2=6000
+declare -i season_start=5
+declare -i season_end=9
+declare -i percentile=99
+
+for metric in "${metrics[@]}"; do
+for continent in "${continents[@]}"; do
+for scen in "${scens[@]}"; do
+
+:
+#python -m prog.02_processing_CLaGARMi.return_period_plots_processing $slice $year_sim_1 $year_sim_2 $metric $continent $scen $start $end $season_start $season_end $percentile
+
+done; done; done;
+
